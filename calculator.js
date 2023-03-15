d593f5ac199a9b3cd1ebb47d22228e6e7529ae83
w0rst <127072370+w0rst@users.noreply.github.com>
2023-03-15 17:54:11 -0400

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,24 @@
+alert("Made By Horizone")
+
+let display = document.getElementById("display");
+
+function addToDisplay(value) {
+  display.value += value;
+}
+
+function clearDisplay() {
+  display.value = "";
+}
+
+function calculate() {
+  try {
+    let result = eval(display.value);
+    if (isNaN(result) || !isFinite(result)) {
+      display.value = "Error";
+    } else {
+      display.value = result;
+    }
+  } catch (error) {
+    display.value = "Error";
+  }
+}
